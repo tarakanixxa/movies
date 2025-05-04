c04ef266923577ec5eac204f67d5f087190ad7c2
Vitaliy <fezzerfeed7@gmail.com>
2025-05-04 22:53:55 +0300

    додав кнопку забронювати, яка перекидує користувача на сторінку booking

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const MovieCard = ({ movie }) => {
+  const navigate = useNavigate();
+
   return (
-    <div
-      className="movie-slide"
-      style={{ backgroundImage: `url(${movie.poster})` }}
-    >
+    <div className="movie-slide" style={{ backgroundImage: `url(${movie.poster})` }}>
       <div className="movie-blur-overlay" />
 
       <div className="movie-left">
         <img src={movie.poster} alt={movie.title} className="movie-poster" />
         <h2 className="movie-title">{movie.title}</h2>
+        <button onClick={() => navigate(`/booking/${movie.id}`)}>Забронювати</button>
       </div>
 
       <div className="movie-right">
