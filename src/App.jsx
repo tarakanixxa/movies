b99cf7aea6c15f9001fda3c16a7f57e2b6b73083
Vitaliy <fezzerfeed7@gmail.com>
2025-05-14 00:26:57 +0300

    додав контейнер для сповіщень бібліотеки реакт тостіфай

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Booking from './pages/Booking';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => (
   <Router>
+    <ToastContainer position="top-center" autoClose={3000} />
+    
     <Routes>
       <Route path="/" element={<Navigate to="/home" replace />} />
       <Route path="/home" element={<Home />} />
       <Route path="/booking/:title" element={<Booking />} />
-
     </Routes>
   </Router>
 );
